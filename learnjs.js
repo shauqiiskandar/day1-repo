d2fd1966c808513067c2b2afa2cab69c8d834b6c
Shauqi Iskandar <iquahs@gmail.com>
2025-11-11 11:03:29 +0800

    first

---
+++ learnjs.js
@@ -0,0 +1,15 @@
+// Given variables
+let a = 10
+let b = "10"
+let c = true
+
+// // Type your code below
+// Your task is to perform the following comparisons and print the results:
+
+
+
+// Don't change the line below
+console.log(`d = ${d}`)
+console.log(`e = ${e}`)
+console.log(`f = ${f}`)
+console.log(`g = ${g}`)
