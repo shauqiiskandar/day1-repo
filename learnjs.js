25a6fae693967baf3938dbc800617bf24e57ad56
Shauqi Iskandar <iquahs@gmail.com>
2025-11-18 11:13:42 +0800

    if statement

--- learnjs.js
+++ learnjs.js
@@ -1,18 +1,11 @@
-//Type coercion
+let a = 16
+let b = 10
 
-// Given variables
-let num = 42
-let bool = false
-let str = "7"
+// Don't change below this line
+let c = 0;
+if (a >= b && !(b < 10)) {
+    c = 2;
+}
 
-// Type your code below
-let a = String(num);
-let b = Number(bool);
-let c = Number(str);
-let d = num + str;
-
-// Don't change the line below
-console.log(`a = ${a}`)
-console.log(`b = ${b}`)
-console.log(`c = ${c}`)
-console.log(`d = ${d}`)
+c += 1;
+console.log(`c = ${c}`);
