d64ec09f341434f6984d040812369093d8b96793
Shauqi Iskandar <iquahs@gmail.com>
2025-11-18 11:10:17 +0800

    Type coerce

--- learnjs.js
+++ learnjs.js
@@ -1,14 +1,18 @@
-// Initialize variables
-let has_license = true;
-let has_space = false;
-let has_experience = true;
+//Type coercion
 
-// Calculate conditions
-let can_sell_regular_pet = (has_license || has_experience) && has_space;
-let can_sell_exotic_pet = (has_license && has_experience) && has_space;
-let cannot_sell_any_pet = (!(has_license) || !(has_experience)) || !has_space
+// Given variables
+let num = 42
+let bool = false
+let str = "7"
 
-// Don't delete the lines below
-console.log("Can sell regular pet:", can_sell_regular_pet)
-console.log("Can sell exotic pet:", can_sell_exotic_pet)
-console.log("Cannot sell any pet:", cannot_sell_any_pet)
+// Type your code below
+let a = String(num);
+let b = Number(bool);
+let c = Number(str);
+let d = num + str;
+
+// Don't change the line below
+console.log(`a = ${a}`)
+console.log(`b = ${b}`)
+console.log(`c = ${c}`)
+console.log(`d = ${d}`)
